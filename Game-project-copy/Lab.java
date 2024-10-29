505af365bf5c0a3382f5fd65aaa98da286056891
Sofia De Palma <6316086@edu.vaniercollege.qc.ca>
2024-10-29 14:20:54 -0400

    changed greenfoot

---
+++ Game-project-copy/Lab.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Lab here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Lab extends World
+{
+
+    /**
+     * Constructor for objects of class Lab.
+     * 
+     */
+    public Lab()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(700, 556, 1); 
+    }
+}
