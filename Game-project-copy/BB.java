68eeafc7dfe8efb6521887d4501fdba94bf59a1f
Sofia De Palma <6316086@edu.vaniercollege.qc.ca>
2024-10-29 09:26:20 -0400

    added greenfoot

---
+++ Game-project-copy/BB.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class BB here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BB extends Actor
+{
+    /**
+     * Act - do whatever the BB wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
