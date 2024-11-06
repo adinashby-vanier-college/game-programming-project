a273881278083da0cb756393092f6c40ac6582a9
KaciL4 <kiluu07@outlook.com>
2024-11-05 22:37:57 -0500

    Added and edited game

---
+++ Game-project-copy/Lab1.java
@@ -0,0 +1,19 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class Lab1 extends World
+{
+
+    /**
+     * Constructor for objects of class Lab1.
+     */
+    public Lab1()
+    {
+        super(600, 400, 1);
+    }
+}
