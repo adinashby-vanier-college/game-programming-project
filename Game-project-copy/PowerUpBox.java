4f6850b8758f34246b13f4aa054c6c0acc593ac8
Mikhayla de las Alas <mikhayladiandra@gmail.com>
2024-11-21 15:00:01 -0500

    Coded power up boxes

--- Game-project-copy/PowerUpBox.java
+++ Game-project-copy/PowerUpBox.java
@@ -1,4 +1,3 @@
-// WARNING: This file is auto-generated and any changes to it will be overwritten
 import lang.stride.*;
 import java.util.*;
 import greenfoot.*;
@@ -8,11 +7,26 @@ import greenfoot.*;
  */
 public class PowerUpBox extends Actor
 {
+    private int hitCount = 0;
 
     /**
      * Act - do whatever the PowerUpBox wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
+        if (isTouching(BananaBullet.class)){
+            hitCount++;
+            removeTouching(BananaBullet.class);
+        }
+        if (hitCount>=2){
+            if (Greenfoot.getRandomNumber(100) < 65){
+                getWorld().addObject(new Banana(), getX(), getY());
+            }
+            else{
+                getWorld().addObject(new Vial(), getX(), getY());
+            }
+            getWorld().removeObject(this);
+        }
+        
     }
 }
--- Game-project-copy/Security.java
+++ Game-project-copy/Security.java
@@ -24,7 +24,9 @@ public class Security extends Actor
             }
         
         }
+
     }
+
 
     /**
      * 
